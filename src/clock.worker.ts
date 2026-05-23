cdecb811c5dace7dcd2b65f92217affa9a0f9061
ImagineFoxes <116079521+ImagineFoxes@users.noreply.github.com>
2026-05-23 12:36:33 +0100

    Refactor clock worker for interval-based updates
    Attempting to fix bug on android (timer stops working in background)

--- src/clock.worker.ts
+++ src/clock.worker.ts
@@ -1,46 +1,30 @@
-let running: boolean = false
-let start: number | undefined
-let startTime: number | undefined
-let interval: number
-let prev: number | undefined
-let lowFps: boolean = false
+let running = false
+let intervalId: number | undefined
+let lowFps = false
+let prev = 0
 
-const tick = (t: number): void => {
-    if (running) {
-        if (start === undefined) {
-            start = t
-            prev = t
-            requestAnimationFrame(tick)
-        } else {
-            interval += t - prev!
-            if (lowFps) {
-                if (interval >= 1000) {
-                    self.postMessage(interval)
-                    interval = 0
-                }
-            } else {
-                self.postMessage(t - prev!)
-            }
-            requestAnimationFrame(tick)
-
-            prev = t
-        }
-    }
-}
-
-self.onmessage = async ({ data }) => {
+self.onmessage = ({ data }) => {
     if (data.start) {
         lowFps = data.lowFps
+
         if (!running) {
             running = true
-            interval = 0
-            startTime = new Date().getTime()
-            requestAnimationFrame(tick)
+            prev = Date.now()
+
+            const delay = lowFps ? 1000 : 16
+
+            intervalId = setInterval(() => {
+                const now = Date.now()
+                const delta = now - prev
+                prev = now
+                self.postMessage(delta)
+            }, delay)
         }
     } else {
         running = false
-        start = undefined
-        startTime = undefined
-        prev = undefined
+        if (intervalId) {
+            clearInterval(intervalId)
+            intervalId = undefined
+        }
     }
 }
